c683e3c72e9fd01f59d0ce3e7bca2b95a7024602
Jeffrey de Wit <Jeffrey.deWit@gmail.com>
2012-08-24 23:02:45 -0400

    Added short description
    Makes code more readable by itself

--- problems/problem-1.php
+++ problems/problem-1.php
@@ -7,7 +7,8 @@
 	</header>
 	<div role="main">
 		<?php echo getProblemDescription(1); 
-		
+			// Problem: Find the sum of all the multiples of 3 or 5 below 1000.
+			
 			// Easy
 			$i = 1;
 			$sum = 0;
